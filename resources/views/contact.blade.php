ca6d4be46322fe9ecb3079cb970e50ddc623bd03
Magecode <magecodedeveloper@Magecodes-MacBook-Pro.local>
2020-11-17 17:18:39 +0200

    Init commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+<div class="content">
+    <div class="title m-b-md">
+        Contact Us
+    </div>
+    <div class="links">
+        <a href="http://laravel.local/">Home</a>
+        <a href="http://laravel.local/about-us">About us</a>
+        <a href="http://laravel.local/contact-us">Contact us</a>
+    </div>
+    <br />
+    <div>
+    <form>
+
+        <label for="fname">First Name</label>
+        <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+        <label for="lname">Last Name</label>
+        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+        <label for="country">Country</label>
+        <select id="country" name="country">
+            <option value="Latvia">Latvia</option>
+            <option value="Lithuania">Lithuania</option>
+            <option value="Estonia">Estonia</option>
+        </select>
+        <br />
+        <br />
+        <label for="subject">Subject</label>
+        <br />
+        <textarea id="subject" name="subject" placeholder="Write something.." style="height:100px; width:250px;"></textarea>
+        <br />
+        <input type="submit" value="Submit">
+
+    </form>
+</div>
+</div>
+</body>
+</html>
